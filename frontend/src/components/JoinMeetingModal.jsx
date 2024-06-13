be017cc30fe4f214207f3902dc6cd2404531ef06
Nabin Singh <nabin@skilluplabs.com.au>
2024-06-13 19:39:29 +1000

    Add zoom

---
+++ frontend/src/components/JoinMeetingModal.jsx
@@ -0,0 +1,6 @@
+function JoinMeeting() {
+    return (
+        <h2>Join Meeting</h2>
+    )
+}
+export default JoinMeeting
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,6 @@
+function SignUp() {
+    return (
+        <h2>Sign Up</h2>
+    )
+}
+export default SignUp
