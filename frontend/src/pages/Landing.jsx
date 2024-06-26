a95c2c0ac2af0222327fecc61adc53fced06ee58
Nabin Singh <nabin@skilluplabs.com.au>
2024-06-26 19:14:19 +1000

    Add landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,18 @@
+import { Link } from 'react-router-dom';
+
 function Landing () {
     return (
-        <h1>Landing</h1>
+        <div className='h-screen flex flex-col justify-center items-center bg-blue'>
+            <div className='flex flex-col gap-20'>
+                <div className='flex justify-center'><img  src="https://st3.zoom.us/static/6.3.22759/image/home2/logo_ZM_Products.png" alt="Zoom Logo" /></div>
+                <div className='bg-white p-8 flex flex-col gap-3 rounded-2xl w-96'>
+                    <Link className='bg-blue p-2 rounded-xl text-white text-lg border border-blue text-center hover:bg-blue/90 transition-all' to="/join-a-meeting">Join a meeting</Link>
+                    <Link className='bg-white p-2 rounded-xl text-black text-lg border border-black  text-center hover:bg-slate-100 transition-all' to="/sign-up">Sign up</Link>
+                    <Link className='bg-wite p-2 rounded-xl text-black text-lg border border-black  text-center hover:bg-slate-100 transition-all' to="/sign-in">Sign in</Link> 
+                </div>
+            </div>
+            <p className='text-white fixed bottom-0 text-sm'>Version: 1.0.0</p>
+        </div>
     )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      blue: '#0b5cff'
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
